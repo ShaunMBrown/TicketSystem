b68b9e09c60b19701a75e0b5eee4ca3304bb325e
ShaunMBrown <ShaunMBrown@gmail.com>
2014-10-28 15:56:52 -0700

    Session updates - Shaun

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   
-  get 'sessions/new'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
 
   get 'welcome/index'
   get 'ct/CTLanding'
